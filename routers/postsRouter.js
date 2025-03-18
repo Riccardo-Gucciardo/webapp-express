1e73f012eb1484643fea09d0cfcdb588a2e84382
Riccardo-Guvviardo <gucciardoriccardo003@gmail.com>
2025-03-18 15:34:23 +0100

    correzioni

--- routers/postsRouter.js
+++ routers/postsRouter.js
@@ -2,14 +2,14 @@ const express = require('express')
 const router = express.Router();
 
 const postController = require("../controllers/postController.js")
-const {index} = postController;
+const {index,show,store,update,patch,destroy} = postController;
 
 router.get('/', index);
-// router.get('/:id', show );
-// router.post('/', store);
-// router.put('/:id', update);
-// router.patch('/:id', patch);
-// router.delete('/:id', destroy);   
+router.get('/:id', show );
+router.post('/', store);
+router.put('/:id', update);
+router.patch('/:id', patch);
+router.delete('/:id', destroy);   
 
 module.exports = router;
  
